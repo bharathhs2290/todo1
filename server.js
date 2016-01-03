6c6e2bcbdbbd9a35d00344a17226324ea7fc6d6f
Bharath HS <bharathhs2290@gmail.com>
2016-01-04 03:50:45 +0530

    post with input filter using underscore

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -31,16 +33,20 @@ app.get('/todos',function(req,res){
 app.get('/todos/:id',function(req,res){
 
     var todoid = parseInt(req.params.id);
-    var matchedid;
 
-    todos.forEach(function(listcheck){
-        if(listcheck.id===todoid)
-        {
-            matchedid=listcheck;
+    var  matchedid = _.findWhere(todos,{id:todoid});
 
-        }
 
-    });
+    //var matchedid;
+    //
+    //todos.forEach(function(listcheck){
+    //    if(listcheck.id===todoid)
+    //    {
+    //        matchedid=listcheck;
+    //
+    //    }
+    //
+    //});
     if(matchedid){
         res.json(matchedid);   }
         else{
@@ -53,7 +59,14 @@ app.get('/todos/:id',function(req,res){
 
 app.post('/todos',function(req,res){
 
-var body =req.body;
+var body = _.pick(req.body,'description','completed');
+
+    if(!_.isBoolean(body.completed)|| !_.isString(body.description) || body.description.trim().length===0){
+
+        return res.status(400).send();
+
+    }
+body.description=body.description.trim();
     body.id=todoNextID++;
 
     todos.push(body);
