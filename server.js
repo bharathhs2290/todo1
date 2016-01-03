bdba961c39f706e102027151ba70c084bd0e3f96
Bharath HS <bharathhs2290@gmail.com>
2016-01-04 02:16:13 +0530

    add todos & Todos id

--- server.js
+++ server.js
@@ -6,12 +6,63 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos =[{
+    description : 'meet mom for lunch',
+    completed:false,
+    id:1
+},
+    {
+        description:'Go to Market',
+        completed:false,
+        id:2
+    },
+    {
+
+        description:'buy Milk',
+        completed:true,
+        id:3
+
+
+
+    }];
+
 
 app.get('/',function(req,res){
 
     res.send('ToDo API Root');
 
 });
+
+app.get('/todos',function(req,res){
+
+   res.json(todos);
+
+});
+
+
+app.get('/todos/:id',function(req,res){
+
+    var todoid = parseInt(req.params.id);
+    var matchedid;
+
+    todos.forEach(function(listcheck){
+        if(listcheck.id===todoid)
+        {
+            matchedid=listcheck;
+
+        }
+
+    });
+    if(matchedid){
+        res.json(matchedid);   }
+        else{
+        res.status(404).send();}
+
+
+   // res.send('ID Requested : '+ req.params.id);
+
+});
+
 
 app.listen(PORT,function(){
     console.log(' server started on port ' + PORT);
